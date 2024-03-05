55b99422d899eb2cf56d472f85307108b6ceccf2
gaetanof <franciscogaetano1@gmail.com>
2024-03-04 23:15:56 -0300

    finish footer

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,9 +1,5 @@
-import Newsletter from './Footer/Newsletter';
-import FooterOverlay from './Footer/FooterOverlay';
 import Navbar from './Navbar/Navbar';
 
 export {
-  Newsletter,
-  FooterOverlay,
   Navbar,
 };
--- client/src/container/Footer/Footer.jsx
+++ client/src/container/Footer/Footer.jsx
@@ -1,54 +1,25 @@
 import React from 'react';
 import { FiFacebook, FiTwitter, FiInstagram } from 'react-icons/fi';
-
-import { FooterOverlay, Newsletter } from '../../components';
 import { images } from '../../constants';
 import './Footer.css';
 
 const Footer = () => (
-  <div className="app__footer section__padding" id="login">
-    <FooterOverlay />
-    <Newsletter />
-
-    <div className="app__footer-links">
-      <div className="app__footer-links_contact">
-        <h1 className="app__footer-headtext">Contact Us</h1>
-        <p className="p__raleway">9 W 53rd St, New York, NY 10019, USA</p>
-        <p className="p__raleway">+1 212-344-1230</p>
-        <p className="p__raleway">+1 212-555-1230</p>
-      </div>
-
-      <div className="app__footer-links_logo">
-        <img src={images.cherryPickers} alt="footer_logo" />
-        <p className="p__raleway">
-          &quot;The best way to find yourself is to lose yourself in the service
-          of others.&quot;
-        </p>
-        <img
-          src={images.spoon}
-          className="spoon__img"
-          style={{ marginTop: 15 }}
-        />
-        <div className="app__footer-links_icons">
-          <FiFacebook />
-          <FiTwitter />
-          <FiInstagram />
-        </div>
-      </div>
-
-      <div className="app__footer-links_work">
-        <h1 className="app__footer-headtext">Working Hours</h1>
-        <p className="p__raleway">Monday-Friday:</p>
-        <p className="p__raleway">08:00 am - 12:00 am</p>
-        <p className="p__raleway">Saturday-Sunday:</p>
-        <p className="p__raleway">07:00 am - 11:00 pm</p>
-      </div>
+  <footer className="app__bg section__padding" id="login">
+    <div className="app__footer-links_icons">
+      <a href="https://www.facebook.com">
+        <FiFacebook />
+      </a>
+      <a href="https://www.twitter.com">
+        <FiTwitter />
+      </a>
+      <a href="https://www.instagram.com">
+        <FiInstagram />
+      </a>
     </div>
-
     <div className="footer__copyright">
-      <p className="p__raleway">2021 cherryPickers. All Rights reserved.</p>
+      <p>2024 CHERRY PICKERS. All Rights reserved.</p>
     </div>
-  </div>
+  </footer>
 );
 
 export default Footer;
