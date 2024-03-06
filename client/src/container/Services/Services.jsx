f876f273648a2c8aebf70278dc3febd221f71ea4
gaetanof <franciscogaetano1@gmail.com>
2024-03-05 23:10:40 -0300

    add sweet alert and detailed fixes

--- client/src/container/Services/Services.jsx
+++ client/src/container/Services/Services.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import './Services.css';
 import images from '../../constants/images';
+import { BsPersonWorkspace } from 'react-icons/bs';
+import { MdOutlineCleaningServices } from 'react-icons/md';
+import { IoCallOutline } from 'react-icons/io5';
+import { TfiAgenda } from 'react-icons/tfi';
 
 const Services = () => (
   <div className="app__bg flex__center section__padding_cards" id="services">
     <div className="container-cards">
       <div className="services-card">
-        <img src={images.servicesConsult} alt="app__logo" />
+        <BsPersonWorkspace className="services-card-icon" />
         <h1>Asesoría</h1>
         <ul>
           <li>Evaluación de la propiedad</li>
@@ -15,7 +19,7 @@ const Services = () => (
         </ul>
       </div>
       <div className="services-card">
-        <img src={images.servicesCleaning} alt="app__logo" />
+        <MdOutlineCleaningServices className="services-card-icon" />
         <h1>Limpieza y mantenimiento</h1>
         <ul>
           <li>Limpieza integral</li>
@@ -23,7 +27,7 @@ const Services = () => (
         </ul>
       </div>
       <div className="services-card">
-        <img src={images.servicesCustomer} alt="app__logo" />
+        <IoCallOutline className="services-card-icon" />
         <h1>Atención al cliente</h1>
         <ul>
           <li>Check-in / Check-out</li>
@@ -31,7 +35,7 @@ const Services = () => (
         </ul>
       </div>
       <div className="services-card">
-        <img src={images.servicesBooking} alt="app__logo" />
+        <TfiAgenda className="services-card-icon" />
         <h1>Gestión de reservas y precios</h1>
         <ul>
           <li>Optimización de tarifas y disponibilidad</li>
