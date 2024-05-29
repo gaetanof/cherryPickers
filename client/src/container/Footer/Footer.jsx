431c7527a05390a700a398c8fd8c4c978b91dcaa
gaetanof <franciscogaetano1@gmail.com>
2024-05-28 23:42:28 -0300

    added wpp & img

--- client/src/container/Footer/Footer.jsx
+++ client/src/container/Footer/Footer.jsx
@@ -1,19 +1,28 @@
 import React from 'react';
 import { FiInstagram } from 'react-icons/fi';
+import { FaWhatsapp } from 'react-icons/fa';
 import './Footer.css';
 
 const Footer = () => (
-  <footer className="app__footer-bg section__padding_footer" id="footer">
+  <footer className="section__padding_footer" id="footer">
     <div className="app__footer-links_icons">
-      <p className="footer_igCta">Seguinos en Instagram</p>
-      <a href="https://www.instagram.com/_cherrypickers?igsh=MWtrbHlsajAwZGxwbg==">
-        <FiInstagram />
-      </a>
-    </div>
-    <div className="footer__copyright">
-      <p className="footer_copyright_paragraph">
-        2024 CHERRY PICKERS. All rights reserved.
-      </p>
+      <div className="footer_contact_method">
+        <p className="footer_igCta">Seguinos en Instagram</p>
+        <a href="https://www.instagram.com/_cherrypickers?igsh=MWtrbHlsajAwZGxwbg==">
+          <FiInstagram />
+        </a>
+      </div>
+      <div className="footer_contact_method">
+        <p className="footer_igCta">WhatsApp</p>
+        <a href="https://wa.me/5491123386795?text=Quiero información de su servicio">
+          <FaWhatsapp />
+        </a>
+      </div>
+      <div className="footer__copyright">
+        <p className="footer_copyright_paragraph">
+          2024 CHERRY PICKERS. All rights reserved.
+        </p>
+      </div>
     </div>
   </footer>
 );
